ebfacc8a1b702c6c0032d4953e2621c8cf206a81
linhhm24 <linh.hm24@gmail.com>
2020-02-24 22:37:07 +0700

    Add file

---
+++ app.js
@@ -0,0 +1,9 @@
+let i;
+for (i=0;i<=100;i++){
+    if (i!== 99){
+        document.write(i + "<br>");
+    }else {
+        document.write(i + "<br>");
+        alert(+ i + " Da hoan thanh");
+    }
+}
